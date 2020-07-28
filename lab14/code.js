8d1bd632380e33c5939e67f9c309e1bf5f4e12cf
selvi <selvimand0@gmail.com>
2020-07-27 19:16:37 -0700

    hi

--- lab14/code.js
+++ lab14/code.js
@@ -11,3 +11,49 @@ add = function(){
     jQuery('#addbutton').click(add);
   }
   jQuery(document).ready(setup);
+
+  // Function of subtraction button
+  Subtraction = function(){
+
+    result = parseInt(jQuery('#op1').html()) + parseInt(jQuery(' #op2').html());
+    //jQuery(' #operand_1').html('123213213213');
+    //alert(result);
+    text = jQuery('#op1').html() + " + " + jQuery('#op2').html() + " = "
+    jQuery('#result').html(text + result);
+  }
+    setup=function(){
+      
+      jQuery('#subbutton').click(subtraction);
+    }
+    jQuery(document).ready(setup);
+
+    //Function of Multiplication button
+    Multiplication = function(){
+
+      result = parseInt(jQuery('#op1').html()) + parseInt(jQuery(' #op2').html());
+      //jQuery(' #operand_1').html('123213213213');
+      //alert(result);
+      text = jQuery('#op1').html() + " + " + jQuery('#op2').html() + " = "
+      jQuery('#result').html(text + result);
+    }
+      setup = function(){
+
+        jQuery('#mulbutton').click(Multiplication);
+      }
+      jQuery(document).ready(setup);
+
+      // function of a division button
+
+      division = function(){
+
+        result = parseInt(jQuery('#op1').html()) + parseInt(jQuery(' #op2').html());
+        //jQuery(' #operand_1').html('123213213213');
+        //alert(result);
+        text = jQuery('#op1').html() + " + " + jQuery('#op2').html() + " = "
+        jQuery('#result').html(text + result);
+      }
+        setup=function(){
+
+          jQuery('#divbutton').click(division);
+        }
+        jQuery(document).ready(setup);
