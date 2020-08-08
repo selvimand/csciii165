fe300f9a5588bd0abe79f375596bdf44d58844b8
selvi <selvimand0@gmail.com>
2020-08-08 11:48:51 -0700

    hi

--- lab14/code.js
+++ lab14/code.js
@@ -2,54 +2,53 @@ add = function(){
 
   result = parseInt(jQuery('#op1').html()) + parseInt(jQuery(' #op2').html());
 
-  //jQuery(' #operand_1').html('123213213213');
-  //alert(result);
+
   text = jQuery('#op1').html() + " + " + jQuery('#op2').html() + " = "
+
   jQuery('#result').html(text + result);
 }
   setup=function(){
     jQuery('#addbutton').click(add);
   }
-  jQuery(document).ready(setup);
+  jQuery(document0.ready(setup);
 
   // Function of subtraction button
   subtraction = function(){
 
-    result = parseInt(jQuery('#op1').html()) + parseInt(jQuery(' #op2').html());
-    //jQuery(' #operand_1').html('123213213213');
-    //alert(result);
-    text = jQuery('#op1').html() + " + " + jQuery('#op2').html() + " = "
+    result = parseInt(jQuery('#op1').html())- parseInt(jQuery(' #op2').html());
+
+    text = jQuery('#op1').html() + " - " + jQuery('#op2').html() + " = "
     jQuery('#result').html(text + result);
   }
     setup=function(){
+      // alert ("hi");
 
       jQuery('#subbutton').click(subtraction);
     }
     jQuery(document).ready(setup);
 
-    //Function of Multiplication button
+    //Function of multiplication button
     multiplication = function(){
 
-      result = parseInt(jQuery('#op1').html()) + parseInt(jQuery(' #op2').html());
-      //jQuery(' #operand_1').html('123213213213');
-      //alert(result);
-      text = jQuery('#op1').html() + " + " + jQuery('#op2').html() + " = "
+      result = parseInt(jQuery('#op1').html()) * parseInt(jQuery(' #op2').html());
+
+      text = jQuery('#op1').html() + " * " + jQuery('#op2').html() + " = "
       jQuery('#result').html(text + result);
     }
       setup = function(){
+        // alert ("hi");
 
         jQuery('#mulbutton').click(multiplication);
       }
       jQuery(document).ready(setup);
 
-      // function of a division button
+      // Function of a division button
 
       division = function(){
 
         result = parseInt(jQuery('#op1').html()) + parseInt(jQuery(' #op2').html());
-        //jQuery(' #operand_1').html('123213213213');
-        //alert(result);
-        text = jQuery('#op1').html() + " + " + jQuery('#op2').html() + " = "
+
+        text = jQuery('#op1').html() + " / " + jQuery('#op2').html() + " = "
         jQuery('#result').html(text + result);
       }
         setup=function(){
---
+++ lab15/first-code.js
@@ -0,0 +1,64 @@
+add = function(){
+
+  result = parseInt(jQuery('#operand1').val()) + parseInt(jQuery(' #operand2').val());
+  text = jQuery('#operand1').val() + " + " + jQuery('#operand2').val() + " = "
+  result = text + result;
+    jQuery('#result').text(result);
+    old_div_context = jQuery('#history').html();
+    result ="<span id='add_style'>"+ result+"</span>";
+    new_div_content = old_div_content + result+ '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+
+
+minus = function(){
+
+  result = parseInt(jQuery('#operand1').val()) - parseInt(jQuery(' #operand2').val());
+  text = jQuery('#operand1').val() + " - " + jQuery('#operand2').val() + " = "
+  result = text + result;
+    jQuery('#result').text(result);
+    old_div_context = jQuery('#history').html();
+    result ="<span id='min_style'>"+ result+"</span>";
+    new_div_content = old_div_content + result+ '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+
+multiply = function(){
+
+  result = parseInt(jQuery('#operand1').val()) * parseInt(jQuery(' #operand2').val());
+  text = jQuery('#operand1').val() + " * " + jQuery('#operand2').val() + " = "
+  result = text + result;
+    jQuery('#result').text(result);
+    old_div_context = jQuery('#history').html();
+    result ="<span id='mul_style'>"+ result+"</span>";
+    new_div_content = old_div_content + result+ '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+
+divide = function(){
+
+  result = parseInt(jQuery('#operand1').val()) / parseInt(jQuery(' #operand2').val());
+  text = jQuery('#operand1').val() + " / " + jQuery('#operand2').val() + " = "
+  result = text + result;
+    jQuery('#result').html(result);
+    old_div_context = jQuery('#history').html();
+    result ="<span id='div_style'>"+ result+"</span>";
+    new_div_content = old_div_content + result+ '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+show = function(){
+  jQuery('#history').show()
+  }
+  hide= function(){
+    jQuery('#history').hide()
+  }
+  setup = function(){
+    jQuery('#addbutton').click(add);
+      jQuery('#subbutton').click(minus);
+        jQuery('#mulbutton').click(multiply);
+          jQuery('#divbutton').click(divide);
+            jQuery('#show_button').click(show);
+            jQuery('#hide_button').click(hide);
+  }
+
+jQuery(document).ready(setup);
